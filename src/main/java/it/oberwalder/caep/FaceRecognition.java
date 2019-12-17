5bcd7ef0f217b7c4fcca811045bdbd902eddfb2f
Mario Oberwalder <mario.oberwalder@gmail.com>
2019-12-17 21:39:36 +0100

    -introduced ImageSource class -video parameters are now read from file and collected in ImageSource

--- src/main/java/it/oberwalder/caep/FaceRecognition.java
+++ src/main/java/it/oberwalder/caep/FaceRecognition.java
@@ -22,50 +22,54 @@ import static org.opencv.highgui.HighGui.*;
 import static org.opencv.imgproc.Imgproc.rectangle;
 
 public class FaceRecognition {
-    private static final Integer NEURAL_NET_FRAME_HEIGHT = 300;
-    private static final Integer NEURAL_NET_FRAME_WIDTH = 300;
+    static final Integer NEURAL_NET_FRAME_HEIGHT = 300;
+    static final Integer NEURAL_NET_FRAME_WIDTH = 300;
     private static final double CONFIDENCE_THRESHOLD = 0.8;
     private static final String NEURAL_NET_MODEL_PATH = "C:\\opencv\\sources\\samples\\dnn\\face_detector\\res10_300x300_ssd_iter_140000_fp16.caffemodel";
     private static final String NEURAL_NET_CONFIG_PATH = "C:\\opencv\\sources\\samples\\dnn\\face_detector\\deploy.prototxt";
-    private static final String TEST_MOVIE_PATH = "C:\\movies\\";
-    private static final String TEST_MOVIE_FILENAME = "test2.mp4";
-    Integer frameWidthFactor;
-    Integer frameHeightFactor;
+
+    ImageSource imageSource = new ImageSource();
     VideoWriter videoOut;
 
-    public FaceRecognition() {};
+    public FaceRecognition() {
+    }
+
+    ;
 
     public int startRecognition() {
-    Mat imageArray = new Mat();
-
-    VideoCapture videoDevice = new VideoCapture(TEST_MOVIE_PATH+TEST_MOVIE_FILENAME);
-    videoDevice.open(TEST_MOVIE_PATH+TEST_MOVIE_FILENAME);
-    Net net = readNetFromCaffe(NEURAL_NET_CONFIG_PATH, NEURAL_NET_MODEL_PATH);
-
+        Mat imageArray = new Mat();
+        VideoCapture videoDevice = new VideoCapture();
+        videoDevice.open(imageSource.getFilePath());
+        Integer frameCounter = 0;
+        Net net = readNetFromCaffe(NEURAL_NET_CONFIG_PATH, NEURAL_NET_MODEL_PATH);
 
         if (videoDevice.isOpened()) {
-            int frameCounter = 0;
             namedWindow("Wonderful window", WINDOW_AUTOSIZE);
-            skipFrames(videoDevice, 0);
-            videoOut = new VideoWriter(LocalDateTime.now().getMinute()+".avi"
-                    ,VideoWriter.fourcc('M','J','P','G')
-                    ,videoDevice.get(5)
-                    ,new Size(1920,1080));
 
-            //while (readMultipleFramesUseOnlyLast(imageArray, videoDevice, 1)) {
+            videoOut = new VideoWriter(LocalDateTime.now().getHour() +""+ LocalDateTime.now().getMinute() + ".avi"
+                    , VideoWriter.fourcc('M', 'J', 'P', 'G')
+                    , imageSource.getFps()
+                    , new Size(imageSource.getWidth(), imageSource.getHeight()));
+
             while (videoDevice.read(imageArray)) {
                 List<Mat> subFrames;
                 List<Mat> detectionMats;
-                subFrames = splitFrameforNet(videoDevice, imageArray);
+                subFrames = splitFrameForNet(videoDevice, imageArray);
+
                 detectionMats = sendImageThroughNet(subFrames, net);
+
                 subFrames = computeResults(subFrames, detectionMats);
+
                 Mat resultImage = buildResultImage(subFrames);
-                showResults(resultImage);
-                Mat videoImage = new Mat();
-                Imgproc.resize(resultImage, videoImage,new Size(1920, 1080));
-                videoOut.write(videoImage);
+
+
+
+                Mat outImage = new Mat(resultImage, new Rect(0, 0, (int) imageSource.getWidth(), (int) imageSource.getHeight()));
+                showResults(outImage);
+                videoOut.write(outImage);
+
                 frameCounter++;
-                if (frameCounter > (int) videoDevice.get(7)){
+                if (frameCounter > imageSource.getFrameCount()) {
                     break;
                 }
             }
@@ -73,55 +77,53 @@ public class FaceRecognition {
             videoOut.release();
             destroyAllWindows();
             return 0;
-    } else {
-        System.out.println("Couldn't open video device");
-    }
+        } else {
+            System.out.println("Couldn't open video device");
+        }
 
 
         return 0;
     }
 
     private boolean readMultipleFramesUseOnlyLast(Mat imageArray, VideoCapture videoDevice, int counter) {
-        for (int i = 0; i < counter-1; i++) {
+        for (int i = 0; i < counter - 1; i++) {
             videoDevice.read(imageArray);
         }
         return videoDevice.read(imageArray);
     }
 
     private Mat buildResultImage(List<Mat> subFrames) {
-        Mat outMat = new Mat(NEURAL_NET_FRAME_HEIGHT*frameHeightFactor, NEURAL_NET_FRAME_WIDTH*frameWidthFactor, subFrames.get(0).type());
+        Mat outMat = new Mat(NEURAL_NET_FRAME_HEIGHT * imageSource.getVerticalTiles(), NEURAL_NET_FRAME_WIDTH * imageSource.getHorizontalTiles(), subFrames.get(0).type());
         List<Mat> horizontalStrips = new ArrayList<>();
-        for (int i = 0; i < frameHeightFactor; i++) {
+        for (int i = 0; i < imageSource.getVerticalTiles(); i++) {
             horizontalStrips.add(new Mat());
-            hconcat(subFrames.subList(i*frameWidthFactor,(i+1)*frameWidthFactor-1), horizontalStrips.get(i));
+            hconcat(subFrames.subList(i * imageSource.getHorizontalTiles(), ((i + 1) * imageSource.getHorizontalTiles())), horizontalStrips.get(i));
         }
-        vconcat(horizontalStrips,outMat);
-    return outMat;
+        vconcat(horizontalStrips, outMat);
+        return outMat;
     }
 
-    private List<Mat> splitFrameforNet(VideoCapture videoCapture, Mat imageArray) {
-        frameWidthFactor = (int) Math.ceil(videoCapture.get(3)/NEURAL_NET_FRAME_WIDTH);
-        frameHeightFactor = (int) Math.ceil(videoCapture.get(4)/NEURAL_NET_FRAME_HEIGHT);
+    private List<Mat> splitFrameForNet(VideoCapture videoCapture, Mat imageArray) {
         List<Mat> subFrames = new ArrayList<>();
-        imageArray = expandFrameForDivision(imageArray,frameWidthFactor, frameHeightFactor);
+        imageArray = expandFrameForDivision(imageArray, imageSource.getHorizontalTiles(), imageSource.getVerticalTiles());
 
-            for (int j = 0; j < frameHeightFactor; j++) {
-                for (int k = 0; k < frameWidthFactor; k++) {
-                    Rect selectingRectangle = new Rect(k*NEURAL_NET_FRAME_WIDTH, j*NEURAL_NET_FRAME_HEIGHT
-                            ,NEURAL_NET_FRAME_WIDTH,NEURAL_NET_FRAME_HEIGHT);
-                    subFrames.add(new Mat(imageArray,selectingRectangle));
-                }
+        for (int j = 0; j < imageSource.getVerticalTiles(); j++) {
+            for (int k = 0; k < imageSource.getHorizontalTiles(); k++) {
+                Rect selectingRectangle = new Rect(k * NEURAL_NET_FRAME_WIDTH, j * NEURAL_NET_FRAME_HEIGHT
+                        , NEURAL_NET_FRAME_WIDTH, NEURAL_NET_FRAME_HEIGHT);
+                subFrames.add(new Mat(imageArray, selectingRectangle));
             }
+        }
         return subFrames;
     }
 
     private Mat expandFrameForDivision(Mat imageArray, Integer frameWidthFactor, Integer frameHeightFactor) {
-    Mat backGround = new Mat();
-    backGround.create(
-            new Size(frameWidthFactor*NEURAL_NET_FRAME_WIDTH
-                    ,frameHeightFactor*NEURAL_NET_FRAME_HEIGHT),imageArray.type());
-    imageArray.copyTo(backGround.submat(0,imageArray.rows(),0,imageArray.cols()));
-    return backGround;
+        Mat backGround = new Mat();
+        backGround.create(
+                new Size(frameWidthFactor * NEURAL_NET_FRAME_WIDTH
+                        , frameHeightFactor * NEURAL_NET_FRAME_HEIGHT), imageArray.type());
+        imageArray.copyTo(backGround.submat(0, imageArray.rows(), 0, imageArray.cols()));
+        return backGround;
     }
 
 
@@ -145,16 +147,16 @@ public class FaceRecognition {
     private void showResults(Mat image) {
         Mat outImage;
         outImage = new Mat();
-        Imgproc.resize(image, outImage,new Size(1280, 720));
+        Imgproc.resize(image, outImage, new Size(1280, 720));
         imshow("Wonderful window", outImage);
         waitKey(1);
     }
 
     private List<Mat> sendImageThroughNet(List<Mat> subFrames, Net net) {
         List<Mat> resultMats = new ArrayList<>();
-        for (Mat subFrame:subFrames
-             ) {
-            Mat blob= blobFromImage(subFrame, 1.0, new Size(300, 300), new Scalar(104, 117, 123), false, false);
+        for (Mat subFrame : subFrames
+        ) {
+            Mat blob = blobFromImage(subFrame, 1.0, new Size(300, 300), new Scalar(104, 117, 123), false, false);
             net.setInput(blob);
             resultMats.add(net.forward().clone());
         }
@@ -162,6 +164,6 @@ public class FaceRecognition {
     }
 
     private void skipFrames(VideoCapture videoDevice, int frames) {
-        videoDevice.set(2,frames/videoDevice.get(7));
+        videoDevice.set(2, frames / videoDevice.get(7));
     }
 }
---
+++ src/main/java/it/oberwalder/caep/ImageSource.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2019. Mario Oberwalder mario.oberwalder@gmail.com
+ */
+
+package it.oberwalder.caep;
+
+import org.opencv.videoio.VideoCapture;
+
+public class ImageSource {
+
+    private String filePath;
+    private String fileName;
+    private String fileLocation;
+    private double width;
+    private double height;
+    private double fps;
+    private double frameCount;
+    private Integer horizontalTiles;
+    private Integer verticalTiles;
+    private Integer expandedWidth;
+    private Integer expandedHeight;
+
+    public ImageSource() {
+        this.fileLocation = "C:\\movies\\";
+        this.fileName = "test2.mp4";
+        this.filePath = fileLocation + fileName;
+        calculatePropertiesFromFile();
+    }
+
+    public  ImageSource(String filePath) {
+        this.filePath = filePath;
+        calculatePropertiesFromFile();
+    }
+
+    private void calculatePropertiesFromFile() {
+        VideoCapture videoDevice = new VideoCapture();
+        videoDevice.open(this.getFilePath());
+
+        if(!videoDevice.isOpened()){
+            System.out.println("Couldn't open image source");
+            }
+
+        this.width = videoDevice.get(3);
+        this.height = videoDevice.get(4);
+        this.fps = videoDevice.get(5);
+        this.frameCount = videoDevice.get(7);
+        this.horizontalTiles = (int) Math.ceil(width/FaceRecognition.NEURAL_NET_FRAME_WIDTH);
+        this.verticalTiles = (int) Math.ceil(height/FaceRecognition.NEURAL_NET_FRAME_HEIGHT);
+        this.expandedWidth = FaceRecognition.NEURAL_NET_FRAME_WIDTH*horizontalTiles;
+        this.expandedHeight = FaceRecognition.NEURAL_NET_FRAME_HEIGHT*verticalTiles;
+
+        videoDevice.release();
+
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getFps() {
+        return fps;
+    }
+
+    public double getFrameCount() {
+        return frameCount;
+    }
+
+    public Integer getHorizontalTiles() {
+        return horizontalTiles;
+    }
+
+    public Integer getVerticalTiles() {
+        return verticalTiles;
+    }
+
+    public Integer getExpandedWidth() {
+        return expandedWidth;
+    }
+
+    public Integer getExpandedHeight() {
+        return expandedHeight;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+}
